b0672ff90ef51035db0ff816b8b0f3451e9ac2c9
nycoliver <oliver@oliverfisher.nyc>
2016-07-17 23:02:08 -0400

    https

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 var restify = require('restify');
+var fs = require('fs');
+
+var CERTIFICATE_PATH = '/etc/letsencrypt/live/donate.nycmesh.net/cert.pem'
+var KEY_PATH = '/etc/letsencrypt/live/donate.nycmesh.net/privkey.pem'
 
 function charge(req, res, next) {
   console.log(req)
-  res.send(req);
+  // res.send(req);
   next();
 }
 
-var server = restify.createServer();
-server.get('/charge', charge);
+var server = restify.createServer({
+  certificate: fs.readFileSync(CERTIFICATE_PATH),
+  key: fs.readFileSync(KEY_PATH)
+});
+server.post('/charge', charge);
 
-server.listen(8080, function() {
+server.listen(9090, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
