4b89ff7cf53bba7e5739c6f0ecd9b47344992d5b
nycoliver <oliver@oliverfisher.nyc>
2016-07-18 00:18:25 -0400

    charge subscription

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
+var key = require('./key.json')
+var stripe = require("stripe")(key);
+
 var restify = require('restify');
-var fs = require('fs');
 
-var CERTIFICATE_PATH = '/etc/letsencrypt/live/donate.nycmesh.net/cert.pem'
-var KEY_PATH = '/etc/letsencrypt/live/donate.nycmesh.net/privkey.pem'
+function charge(req, res) {
+  console.log(req.params)
+  if (!req.params || !req.params.stripeToken)
+    return
 
-function charge(req, res, next) {
-  console.log(req)
-  // res.send(req);
-  next();
+  //should really sanitize input
+  var stripeToken = req.params.stripeToken;
+
+  stripe.customers.create({
+    source: stripeToken,
+    plan: "hundred-monthly"
+  }, function(err, customer) {
+    console.log(err, customer)
+  });
 }
 
-var server = restify.createServer({
-  certificate: fs.readFileSync(CERTIFICATE_PATH),
-  key: fs.readFileSync(KEY_PATH)
-});
+var server = restify.createServer();
+
+server.use(restify.bodyParser());
+
 server.post('/charge', charge);
 
 server.listen(9090, function() {
-  console.log('%s listening at %s', server.name, server.url);
+  console.log('listening at %s', server.url);
 });
