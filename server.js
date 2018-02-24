db80fcc01d4fabcf02fd47884825a1e74744eb18
olivernyc <oliver@static.network>
2018-02-24 17:46:52 -0500

    Setup donate endpoint, send success/failure response

--- server.js
+++ server.js
@@ -1,7 +1,61 @@
 var key = require("./key");
 var stripe = require("stripe")(key);
-
 var restify = require("restify");
+
+function donate(req, res) {
+  if (!req.params || !req.params.stripeToken) return;
+
+  // should really sanitize input
+  var stripeToken = req.params.stripeToken;
+  var donationAmount = req.params.donationAmount;
+  var subscriptionPlan = req.params.subscriptionPlan;
+
+  // One time donation
+  if (donationAmount) {
+    createCharge(stripeToken, donationAmount, (err, charge) => {
+      if (err) {
+        console.log("Error charging card: ", err);
+        res.send(400, "Error charging card");
+      } else {
+        console.log("Charged " + charge.receipt_email + " " + donationAmount);
+        res.end(200);
+      }
+    });
+  }
+
+  // Monthly subscription
+  if (subscriptionPlan) {
+    createSubscription(stripeToken, subscriptionPlan, (err, charge) => {
+      if (err) {
+        console.log("Error creating subscription: ", err);
+        res.send(400, "Error creating subscription");
+      } else {
+        console.log("Subscribed " + charge.receipt_email + " to " + subscriptionPlan);
+        res.end(200);
+      }
+    })
+  }
+}
+
+function createCharge(stripeToken, amount, cb) {
+  const charge = {
+    amount: donationAmount,
+    currency: "usd",
+    source: stripeToken.id,
+    description: "NYC Mesh Donation"
+  };
+  stripe.charges.create(charge, cb);
+}
+
+function createSubscription(stripeToken, subscriptionPlan, cb) {
+  const customer = {
+    source: stripeToken.id,
+    plan: subscriptionPlan,
+    description: "NYC Mesh Donation",
+    email: stripeToken.email
+  };
+  stripe.customers.create(customer, cb);
+}
 
 function charge(req, res) {
   if (!req.params || !req.params.stripeToken) return;
@@ -69,7 +123,9 @@ function charge(req, res) {
 }
 
 function subscribeCustomer(customer, plan, trialDays) {
-  const trial_end = parseInt(new Date(Date.now() + (trialDays * 24 * 60 * 60 * 1000)).getTime()/1000);
+  const trial_end = parseInt(
+    new Date(Date.now() + trialDays * 24 * 60 * 60 * 1000).getTime() / 1000
+  );
   stripe.subscriptions.create(
     {
       customer: customer.id,
@@ -96,6 +152,8 @@ var server = restify.createServer();
 server.use(restify.bodyParser());
 
 server.post("/charge", charge);
+
+server.post("/donate", donate);
 
 server.listen(9090, function() {
   console.log("listening at %s", server.url);
